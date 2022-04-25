3ed58a72116003ee5abc839bb981d5ca515487ea
maicon70 <ahmetkagankervan@gmail.com>
2022-04-25 14:12:14 +0300

    Initialize eclipse project

---
+++ src/columns/Main.java
@@ -0,0 +1,9 @@
+package columns;
+
+import enigma.core.Enigma;
+
+public class Main {
+	public static void main(String[] args) {		
+		Enigma.getConsole().getTextWindow().output("Hello world");
+	}
+}
