42bd085e25d1709889b406f96f225e4dd755cdb2
Berkay Dinç <berkayydinc@gmail.com>
2022-05-25 21:20:33 +0300

    Remove class Cursor, move methods to Game

--- src/columns/Game.java
+++ src/columns/Game.java
@@ -1,11 +1,15 @@
 package columns;
 
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.IOException;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
+import enigma.console.TextAttributes;
 import util.SingleLinkedList;
 import util.CardNode;
 import util.ColumnNode;
@@ -45,7 +49,91 @@ public class Game {
 		}
 		
 		Game.initialize();
-		Cursor.initialize();
+
+		// Start the cursor from the first non-empty column.
+		for (int i = 0; i < Game.NUMBER_OF_COLUMNS; i++) {
+			if (Game.getColumn(i).getSize() > 0) {
+				column = i;
+				break;
+			}
+		}
+
+		KeyListener keyListener = new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+			}
+
+			@Override
+			public void keyPressed(KeyEvent event) {
+				int keyCode = event.getKeyCode();
+
+				if (selectionMode) {
+					switch (keyCode) {
+					case KeyEvent.VK_LEFT:
+						selectColumn(false);
+						break;
+					case KeyEvent.VK_RIGHT:
+						selectColumn(true);
+						break;
+					case KeyEvent.VK_X:
+						boolean transferred;
+						if (transferFromBox) {
+ 							transferred = Game.transferLastNumberFromBox(destinationColumn);
+						} else {
+							transferred = Game.transferNumbers(column, row, destinationColumn);	
+ 						}
+						
+ 						if (transferred) exitSelectionMode();
+						Game.playCardTransferringSound();
+						break;
+					case KeyEvent.VK_Z, KeyEvent.VK_B:
+						exitSelectionMode();
+						break;
+					}
+
+				} else {
+					switch (keyCode) {
+					case KeyEvent.VK_UP:
+						moveCursorVertical(true);
+						break;
+					case KeyEvent.VK_DOWN:
+						moveCursorVertical(false);
+						break;
+					case KeyEvent.VK_LEFT:
+						moveCursorHorizontal(true);
+						break;
+					case KeyEvent.VK_RIGHT:
+						moveCursorHorizontal(false);
+						break;
+					case KeyEvent.VK_B: 
+						if (Game.lastboxnumber == 0) {
+							if(Game.getBox().size() != 0) {
+								Game.drawNumberFromBox();
+							}	
+						} else {
+							transferFromBox = true;
+							enterSelectionMode();
+						}
+						break;
+					case KeyEvent.VK_Z:
+						transferFromBox = false;
+						enterSelectionMode();
+						break;
+					case KeyEvent.VK_E:
+						System.exit(0); // user can go back to menu with pressing E, we will redirect the user.
+						break;
+					}					
+				}
+			}
+		};
+
+		// Listen for key events.
+		Display.window.addKeyListener(keyListener);
+		
 		Display.initialize();
 	}
 
@@ -277,9 +365,10 @@ public class Game {
 		}
 	}
 	
+	// ------------------------------------------------------ SOUND RELATED CODE -------------------------------------------------------------
+	
 	private static final String CARD_SOUND_FILE_PATH = "cardsound.wav";
 	private static final String CARD_SOUND_TRANSFERRING_FILE_PATH = "cardtransferringsound.wav";
-	
 	
 	private static Clip cardSoundClip;
 	private static Clip cardSoundClipTransfer;
@@ -291,10 +380,89 @@ public class Game {
 		
 		cardSoundClip.start();
 	}
+	
 	public static void playCardTransferringSound() {
 
 		cardSoundClipTransfer.setFramePosition(0);
 		
 		cardSoundClipTransfer.start();
+	}
+	
+	// ------------------------------------------------------ CURSOR RELATED CODE -------------------------------------------------------------
+
+	private static int column = 0;
+	private static int row = 0;
+	private static boolean selectionMode = false;
+	private static boolean transferFromBox = false;
+	private static int destinationColumn = 0;
+	
+	private static void enterSelectionMode() {
+		selectionMode = true;
+		destinationColumn = column;
+		Display.displayColumnTitle(destinationColumn, new TextAttributes(Color.GREEN));
+	}
+	
+	private static void exitSelectionMode() {
+		selectionMode = false;
+		Display.displayColumnTitle(destinationColumn, new TextAttributes(Color.WHITE));
+	}
+		
+	public static void selectColumn(boolean selectNext) {
+		Display.displayColumnTitle(destinationColumn, new TextAttributes(Color.WHITE));
+		destinationColumn += Game.NUMBER_OF_COLUMNS + (selectNext ? 1 : -1);
+		destinationColumn %= Game.NUMBER_OF_COLUMNS;
+		Display.displayColumnTitle(destinationColumn, new TextAttributes(Color.GREEN));
+	}
+
+	/**
+	 * Moves the cursor vertically. If the limit is exceeded, the cursor starts from
+	 * the opposite-end.
+	 */
+	private static void moveCursorVertical(boolean moveUp) {
+		// Clear the cursor frame.
+		Display.clearCursorFrameAtRowOfColumn(column, row);
+
+		// Move the current row
+		int columnSize = Game.getColumn(column).getSize();
+		row += columnSize + (moveUp ? -1 : 1);
+		row %= columnSize;
+
+		// Draw the cursor frame.
+		Display.displayCursorFrameAtRowOfColumn(column, row);
+	}
+
+	/**
+	 * Moves the cursor horizontally. If the limit is exceeded, the cursor starts
+	 * from the opposite-end.
+	 * <p>
+	 * This method will skip the empty columns.
+	 * <p>
+	 * If the `row` is bigger than the next column's size, the `row` starts from the
+	 * last number of the next column.
+	 */
+	private static void moveCursorHorizontal(boolean moveLeft) {
+		// Clear the cursor frame.
+		Display.clearCursorFrameAtRowOfColumn(column, row);
+
+		// Search for a non-empty column.
+		for (int i = 0; i < Game.NUMBER_OF_COLUMNS; i++) {
+			column += Game.NUMBER_OF_COLUMNS + (moveLeft ? -1 : 1);
+			column %= Game.NUMBER_OF_COLUMNS;
+			int columnSize = Game.getColumn(column).getSize();
+
+			// If a non-empty column is found, select it.
+			if (columnSize > 0) {
+				// If the row is bigger than the next column's,
+				// size, start it from the last element of the next
+				// column.
+				if (row >= columnSize)
+					row = columnSize - 1;
+
+				break;
+			}
+		}
+
+		// Draw the cursor frame.
+		Display.displayCursorFrameAtRowOfColumn(column, row);
 	}
 }
